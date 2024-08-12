4f6d61548b96cf43a88e186507e6ef2348537ddc
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-12 12:07:16 +0530

    value

---
+++ Data Structures and Algorithms/largestarrya.py
@@ -0,0 +1,7 @@
+my_array = [2,45,8,6,7,9]
+laregest_array = my_array[0]
+for i in my_array:
+    if i > laregest_array:
+        laregest_array = i
+        
+print( laregest_array)
---
+++ Data Structures and Algorithms/lowestvalue.py
@@ -0,0 +1,6 @@
+array = [2,34,6,7,90]
+ravi = array[0]
+for i in array:
+    if i < ravi:
+        i = ravi
+print(ravi)
