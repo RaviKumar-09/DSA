e9efd85f7cd2cb71d6109a467da98f71c6f5f5d5
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-12 12:48:23 +0530

    new variable

--- Data Structures and Algorithms/largestarrya.py
+++ Data Structures and Algorithms/largestarrya.py
@@ -2,6 +2,6 @@ my_array = [2,45,8,6,7,9]
 laregest_array = my_array[0]
 for i in my_array:
     if i > laregest_array:
-        laregest_array = i
+        i= laregest_array 
         
 print( laregest_array)
